d584ed15213a23aa5fecfc1bd585bddf29b9ddf6
SergeyNevinskiy <s.nevinskiy1987@ukr.net>
2022-07-17 17:52:46 +0300

    Ok

---
+++ script/script.js
@@ -0,0 +1,33 @@
+'use strict'
+
+// 1
+// for (let i = 1; i <= 100; i++){
+// console.log(i)
+// }
+
+// 2
+// for (let i = 100; i > 0; i--){
+    // console.log(i)
+// }
+
+// 3
+// const arr = [1, 2, 3, 4, 5];
+// let sum = null;
+
+// 4
+// for(let i = 0; i < arr.length; i++){
+//     sum += arr[i]    
+// };
+
+// console.log(sum);
+
+
+// 5
+// const arr = [1, 2, 3, 4, 5];
+// let sum = null;
+
+// for( let i = 0; i < arr.length; i++) {
+//     sum += arr[i]**2;
+// }
+
+// console.log(sum)
